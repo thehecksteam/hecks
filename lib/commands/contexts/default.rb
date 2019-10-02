b7bfc5f68ae3a086d4e0620cd941796d644346af
Chris Young <beesucker@gmail.com>
2019-10-02 14:09:26 -0700

    first stab at thor generators

--- lib/commands/contexts/default.rb
+++ lib/commands/contexts/default.rb
@@ -2,10 +2,13 @@ class Hecks < Thor
   package_name 'Hecks'
   HECKS_FILE_NAME = 'Domainfile'.freeze
 
-  desc 'new', 'Create a new domain project with some supporting files.  Especially the Domainfile'
+  desc 'new', 'Create a new domain project with some supporting files.  '\
+              'Especially the Domainfile'
   method_option :domain_name, type: :string, aliases: '-d'
   def new
     puts "Implement me"
   end
-end
 
+  desc 'foo', 'generators'
+  subcommand 'foo', HecksDomain::Commands::GenerateCommands
+end
